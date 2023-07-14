0bc3c8ec18004d577b3c834dc711d7e004ae1ac3
dhdhxk <dhdhxk@hotmail.com>
2023-07-14 12:04:01 -0700

    Finish Crud

--- app/RiserAcademicAPI/v1/urls.py
+++ app/RiserAcademicAPI/v1/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path("course/<int:pk>", views.CourseViewSet.as_view(), name="course_detail"),
     path("subject", views.SubjectViewSet.as_view(), name="subject_index"),
     path("subject/<int:pk>", views.SubjectViewSet.as_view(), name="subject_detail"),
+    path("assignment", views.AssignmentViewSet.as_view(), name="assignment_index"),
+    path("assignment/<int:pk>", views.AssignmentViewSet.as_view(), name="assignment_detail"),
+    path("assignment/<int:pk>/submit", views.SubmissionViewSet.as_view(), name="submission_index"),
+    path("submission/<int:pk>", views.SubmissionViewSet.as_view(), name="submission_index"),
+    path("grade", views.GradeViewSet.as_view(), name="subject_index"),
+    path("grade/<int:pk>", views.GradeViewSet.as_view(), name="subject_detail"),
 ]
--- app/RiserAcademicAPI/v1/views.py
+++ app/RiserAcademicAPI/v1/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from RiserAcademicAPI.models import School, Course, Subject
-from .serializers import SchoolSerializer, CourseSerializer, SubjectSerializer
+from RiserAcademicAPI.models import School, Course, Subject, Assignment, Submission, Grade
+from .serializers import SchoolSerializer, CourseSerializer, SubjectSerializer,\
+    AssignmentSerializer, SubmissionSerializer, GradeSerializer
 
 
 # Create your views here.
@@ -117,4 +118,117 @@ class SubjectViewSet(APIView):
     def delete(self, request, pk: int):
         subject = Subject.objects.get(pk=pk)
         subject.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class AssignmentViewSet(APIView):
+    def get(self, request, pk: int = False):
+        try:
+            if pk:
+                assignment = Assignment.objects.get(pk=pk)
+                serializer = AssignmentSerializer(assignment)
+            else:
+                assignment = Assignment.objects.all()
+                serializer = AssignmentSerializer(assignment, many=True)
+            return Response(serializer.data)
+        except:
+            return Response({
+                'error': 'Assignment Does not Exist'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        serializer = AssignmentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk: int):
+        assignment = Assignment.objects.get(pk=pk)
+        serializer = AssignmentSerializer(assignment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk: int):
+        assignment = Assignment.objects.get(pk=pk)
+        assignment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class SubmissionViewSet(APIView):
+    def get(self, request, pk: int = False):
+        try:
+            if pk:
+                submission = Submission.objects.get(pk=pk)
+                serializer = SubmissionSerializer(submission)
+            else:
+                submission = Submission.objects.all()
+                serializer = SubmissionSerializer(submission, many=True)
+            return Response(serializer.data)
+        except:
+            return Response({
+                'error': 'Submission Does not Exist'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        serializer = SubmissionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk: int):
+        submission = Submission.objects.get(pk=pk)
+        serializer = SubmissionSerializer(submission, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk: int):
+        submission = Submission.objects.get(pk=pk)
+        submission.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class GradeViewSet(APIView):
+    def get(self, request, pk: int = False):
+        try:
+            if pk:
+                grade = Grade.objects.get(pk=pk)
+                serializer = GradeSerializer(grade)
+            else:
+                grade = Grade.objects.all()
+                serializer = GradeSerializer(grade, many=True)
+            return Response(serializer.data)
+        except:
+            return Response({
+                'error': 'Grade Does not Exist'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        serializer = GradeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk: int):
+        grade = Grade.objects.get(pk=pk)
+        serializer = GradeSerializer(grade, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk: int):
+        grade = Grade.objects.get(pk=pk)
+        grade.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
