c19e1c1e5b3a05723a446c0c462e558a3056fe57
dhdhxk <dhdhxk@hotmail.com>
2023-11-07 18:25:11 -0800

    Update base.py

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -26,39 +26,8 @@ SECRET_KEY = 'django-insecure-q960005#!zq7=q@dwe=$i25+9g9+&bk_rdythd+#_@pmxp8y*n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
-
-def is_ec2_linux():
-    """Detect if we are running on an EC2 Linux Instance
-       See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
-    """
-    if os.path.isfile("/sys/hypervisor/uuid"):
-        with open("/sys/hypervisor/uuid") as f:
-            uuid = f.read()
-            return uuid.startswith("ec2")
-    return False
-
-
-def get_linux_ec2_private_ip():
-    """Get the private IP Address of the machine if running on an EC2 linux server"""
-    from urllib.request import urlopen
-    if not is_ec2_linux():
-        return None
-    try:
-        response = urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
-        ec2_ip = response.read().decode('utf-8')
-        if response:
-            response.close()
-        return ec2_ip
-    except Exception as e:
-        print(e)
-        return None
-
-
-private_ip = get_linux_ec2_private_ip()
-if private_ip:
-    ALLOWED_HOSTS.append(private_ip)
 
 # Application definition
 
